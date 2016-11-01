fdbe23c6665ed859e1c60019d95b527f5df53f1a
Michael Walker <mike@barrucadu.co.uk>
2016-11-01 01:22:28 +0000

    Allow aborting prompts.

--- Interface/Haskeline.hs
+++ Interface/Haskeline.hs
@@ -21,10 +21,12 @@ import Interface.Utils
 aiPlay :: forall a. (Enum a, Bounded a, Eq a, Read a, Show a) => State a -> IO ()
 aiPlay s0 = runInputT settings $ do
     s' <- initialise s0
-
-    outputStrLn "\nEntering game loop..."
-    help
-    loop s'
+    case s' of
+      Just initialState -> do
+        outputStrLn "\nEntering game loop..."
+        help
+        loop initialState
+      Nothing -> pure ()
   where
     -- the haskeline settings: completion for place names.
     settings = setComplete (completeWord Nothing " \t" completePlace) defaultSettings
@@ -34,10 +36,15 @@ aiPlay s0 = runInputT settings $ do
 
     -- set up the initial game state.
     initialise s = do
-      theHand  <- prompt "Cards in hand: "  readWords
-      theTable <- prompt "Cards on table: " readWords
-
-      doDrawTickets . AI.draw theHand . AI.setCards theTable $ s
+      theHand <- prompt "Cards in hand: " readWords
+      case theHand of
+        Just hand -> do
+          theTable <- prompt "Cards on table: " readWords
+          case theTable of
+            Just table ->
+              doDrawTickets . AI.draw hand . AI.setCards table $ s
+            Nothing -> pure Nothing
+        Nothing -> pure Nothing
 
     -- a brief help message
     help = do
@@ -49,61 +56,74 @@ aiPlay s0 = runInputT settings $ do
     loop s = do
       outputStrLn ""
       (s', continue) <- prompt "ai> " Just >>= \case
-        "s" -> do
+        Just "s" -> do
           let action = AI.suggest s
           outputStrLn (showMove action "\n")
           s' <- case action of
             AI.DrawLocomotiveCard -> doDrawSpecial s
             (AI.DrawCards c1 c2) -> doDraw c1 c2 s
-            (AI.ClaimRoute from to colour cards) -> pure $ doClaim from to colour cards s
+            (AI.ClaimRoute from to colour cards) -> pure . Just $ doClaim from to colour cards s
             AI.DrawTickets -> doDrawTickets s
           pure (s', True)
-        "e" -> do
+        Just "e" -> do
           s' <- doEnemyClaim s
           pure (s', True)
-        "v" -> do
+        Just "v" -> do
           s' <- doSetCards s
           pure (s', True)
-        "p" -> do
+        Just "p" -> do
           printState outputStr s
-          pure (s, True)
-        "h" -> do
+          pure (Nothing, True)
+        Just "h" -> do
           help
-          pure (s, True)
-        "q" -> pure (s, False)
-        _   -> pure (s, True)
+          pure (Nothing, True)
+        Just "q" -> pure (Nothing, False)
+        _   -> pure (Nothing, True)
 
-      when continue $ do
-        printDiff s s'
-        newState <- if s /= s'
-          then do
-            outputStrLn ""
-            accept <- prompt "Accept? [y/n] " (Just . map toLower)
-            pure $ if accept `elem` ["accept", "y", "yes"] then s' else s
-          else pure s
-        loop newState
+      when continue $
+        case s' of
+          Just newState
+            | s /= newState -> do
+              printDiff s newState
+              outputStrLn ""
+              accept <- confirm
+              let realNewState = if accept then newState else s
+              loop realNewState
+            | otherwise -> loop s
+          Nothing -> loop s
 
 -------------------------------------------------------------------------------
 -- Functions
 
 -- | Draw a locomotive
-doDrawSpecial :: State a -> InputT IO (State a)
+doDrawSpecial :: State a -> InputT IO (Maybe (State a))
 doDrawSpecial s = do
-  replacement <- prompt "Replacement card: " readMaybe
-  pure $ replaceDraw Special replacement s
+  mreplacement <- prompt "Replacement card: " readMaybe
+  pure $ do
+    replacement <- mreplacement
+    pure $ replaceDraw Special replacement s
 
 -- | Draw a pair of cards
-doDraw :: Maybe Colour -> Maybe Colour -> State a -> InputT IO (State a)
+doDraw :: Maybe Colour -> Maybe Colour -> State a -> InputT IO (Maybe (State a))
 doDraw (Just c1) (Just c2) s = do
-  replacements <- prompt "Replacement cards: " (readWordsL 2)
-  pure . replaceDraw c2 (replacements !! 1) . replaceDraw c1 (head replacements) $ s
+  mreplacements <- prompt "Replacement cards: " (readWordsL 2)
+  pure $ do
+    replacements <- mreplacements
+    pure . replaceDraw c2 (replacements !! 1) . replaceDraw c1 (head replacements) $ s
 doDraw (Just c) _ s = do
-  fromdeck    <- prompt "Card from deck:   " readMaybe
-  replacement <- prompt "Replacement card: " readMaybe
-  pure . AI.draw [fromdeck] . replaceDraw c replacement $ s
+  mfromdeck  <- prompt "Card from deck: " readMaybe
+  case mfromdeck of
+    Just fromdeck -> do
+      mreplacement <- prompt "Replacement card: " readMaybe
+      pure $ do
+        replacement <- mreplacement
+        pure . AI.draw [fromdeck] . replaceDraw c replacement $ s
+    Nothing -> pure Nothing
 doDraw _ _ s = do
-  fromdeck <- prompt "Cards from deck: " (readWordsL 2)
-  pure $ AI.draw fromdeck s
+  mfromdeck <- prompt "Cards from deck: " (readWordsL 2)
+  pure $ do
+    fromdeck <- mfromdeck
+    pure $ AI.draw fromdeck s
 
 -- | Claim a route.
 doClaim :: Enum a => a -> a -> Colour -> [Colour] -> State a -> State a
@@ -111,31 +131,43 @@ doClaim from to colour cards =
   AI.discard cards . AI.claim from to colour
 
 -- | Draw tickets.
-doDrawTickets :: (Enum a, Read a, Show a) => State a -> InputT IO (State a)
+doDrawTickets :: (Enum a, Read a, Show a) => State a -> InputT IO (Maybe (State a))
 doDrawTickets s = do
-  theTickets <- prompt "Tickets: " readTickets
-  let (keep, plan) = AI.planTickets theTickets s
-  outputStr "\nKeep these tickets: "
-  printList outputStr showTicket "none!" (L.toList keep)
-  let s' = s { AI.pendingTickets = L.toList keep ++ AI.pendingTickets s
-             , AI.plan = plan
-             }
-  pure s'
+  mtickets <- prompt "Tickets: " readTickets
+  case mtickets of
+    Just tickets -> do
+      let (keep, plan) = AI.planTickets tickets s
+      outputStr "\nKeep these tickets: "
+      printList outputStr showTicket "none!" (L.toList keep)
+      let s' = s { AI.pendingTickets = L.toList keep ++ AI.pendingTickets s
+                 , AI.plan = plan
+                 }
+      pure (Just s')
+    Nothing -> pure Nothing
 
 -- | Register an enemy claim.
-doEnemyClaim :: (Enum a, Read a) => State a -> InputT IO (State a)
+doEnemyClaim :: (Enum a, Read a) => State a -> InputT IO (Maybe (State a))
 doEnemyClaim s = do
-  from   <- prompt "From:   " readMaybe
-  to     <- prompt "To:     " readMaybe
-  colour <- prompt "Colour: " readMaybe
-
-  pure $ AI.enemyClaim from to colour s
+  mfrom <- prompt "From:" readMaybe
+  case mfrom of
+    Just from -> do
+      mto <- prompt "To: " readMaybe
+      case mto of
+        Just to -> do
+          mcolour <- prompt "Colour: " readMaybe
+          pure $ do
+            colour <- mcolour
+            pure $ AI.enemyClaim from to colour s
+        Nothing -> pure Nothing
+    Nothing -> pure Nothing
 
 -- | Set the visible cards.
-doSetCards :: State a -> InputT IO (State a)
+doSetCards :: State a -> InputT IO (Maybe (State a))
 doSetCards s = do
-  theTable <- prompt "Cards on table: " readWords
-  pure $ AI.setCards theTable s
+  mtable <- prompt "Cards on table: " readWords
+  pure $ do
+    table <- mtable
+    pure $ AI.setCards table s
 
 -------------------------------------------------------------------------------
 -- Utilities
@@ -148,14 +180,24 @@ replaceDraw original replacement s =
       onTable' = M.alter (\case Just i -> Just (i+1); Nothing -> Just 1) replacement onTable
   in s' { AI.ontable = onTable' }
 
--- | Prompt for input, and parse it.
-prompt :: String -> (String -> Maybe a) -> InputT IO a
+-- | Prompt for input, and parse it. Returns @Nothing@ if no input was given.
+prompt :: String -> (String -> Maybe a) -> InputT IO (Maybe a)
 prompt msg f = go where
   go = do
     input <- getInputLine msg
     case input of
-      Just str -> maybe (outputStrLn "Try again." >> go) pure (f str)
-      Nothing  -> go
+      Just ""  -> pure Nothing
+      Just str -> maybe (outputStrLn "Try again." >> go) (pure . Just) (f str)
+      Nothing  -> pure Nothing
+
+-- | A confirmation message, where anything other than "y" or "yes"
+-- (including empty input) is @False@.
+confirm :: InputT IO Bool
+confirm = do
+  input <- getInputLine "Accept? [y/N] "
+  pure $ case input of
+    Just str -> map toLower str `elem` ["y", "yes"]
+    Nothing  -> False
 
 -- | Print the difference between two states.
 --
