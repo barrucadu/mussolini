cea838569380a0261e0732516e7652eb27b535c9
Michael Walker <mike@barrucadu.co.uk>
2016-11-01 14:50:00 +0000

    Drop the GHCi interface.

--- Interface.hs
+++ Interface.hs
@@ -1,22 +1,22 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-module Interface.Haskeline (aiPlay) where
+module Interface (aiPlay) where
 
 import Control.Monad (when)
 import Data.Char (toLower)
 import Data.List (isPrefixOf, nub)
+import Data.List.NonEmpty (NonEmpty, nonEmpty)
 import qualified Data.List.NonEmpty as L
 import qualified Data.Map as M
 import System.Console.Haskeline (InputT, defaultSettings, getInputLine, runInputT, outputStr, outputStrLn, setComplete)
 import System.Console.Haskeline.Completion (completeWord, simpleCompletion)
 import Text.Read (readMaybe)
 
-import AI (State)
-import Graph (Colour(Special))
+import AI (State, Ticket(..), Move(..))
+import Graph (Colour, Label(..))
 import qualified Graph
 import qualified AI
-import Interface.Utils
 
 -- | An interactive read-eval-print loop for playing as the AI.
 aiPlay :: forall a. (Enum a, Bounded a, Eq a, Read a, Show a) => State a -> IO ()
@@ -67,7 +67,7 @@ aiPlay s0 = runInputT settings $ do
           s' <- doSetCards s
           pure (s', True)
         Just "p" -> do
-          printState outputStr s
+          doPrintState s
           pure (Nothing, True)
         Just "h" -> do
           help
@@ -94,7 +94,7 @@ aiPlay s0 = runInputT settings $ do
 doDrawSpecial :: State a -> InputT IO (Maybe (State a))
 doDrawSpecial s =
   prompt "Replacement card: " readMaybe .>= \replacement ->
-  pure . Just $ replaceDraw Special replacement s
+  pure . Just $ replaceDraw Graph.Special replacement s
 
 -- | Draw a pair of cards
 doDraw :: Maybe Colour -> Maybe Colour -> State a -> InputT IO (Maybe (State a))
@@ -120,7 +120,7 @@ doDrawTickets s =
   prompt "Tickets: " readTickets .>= \tickets -> do
     let (keep, plan) = AI.planTickets tickets s
     outputStr "\nKeep these tickets: "
-    printList outputStr showTicket "none!" (L.toList keep)
+    printList showTicket "none!" (L.toList keep)
     let s' = s { AI.pendingTickets = L.toList keep ++ AI.pendingTickets s
                , AI.plan = plan
                }
@@ -143,20 +143,27 @@ doSetCards s =
   prompt "Cards on table: " readWords .>= \table ->
   pure . Just $ AI.setCards table s
 
+doPrintState :: Show a => State a -> InputT IO ()
+doPrintState s = do
+  outputStr . showString "Remaining trains: " . shows (AI.remainingTrains s) $ "\n"
+  outputStrLn ""
+
+  outputStr "Cards:\n"
+  outputStr "\tIn hand:  " >> printList showCards "none!" (M.toList $ AI.hand    s)
+  outputStr "\tOn table: " >> printList showCards "none!" (M.toList $ AI.ontable s)
+  outputStrLn ""
+
+  outputStr "Tickets:\n"
+  outputStr "\tComplete: " >> printList showTicket "none!" (AI.completedTickets s)
+  outputStr "\tPending:  " >> printList showTicket "none!" (AI.pendingTickets   s)
+  outputStr "\tMissed:   " >> printList showTicket "none!" (AI.missedTickets    s)
+  outputStrLn ""
+
+  printPlan s
+
+
 -------------------------------------------------------------------------------
--- Utilities
-
--- | Like '>>=' but nicely dealing with @Maybe@-values.
-(.>=) :: Monad m => m (Maybe a) -> (a -> m (Maybe b)) -> m (Maybe b)
-mma .>= mmf = mma >>= maybe (pure Nothing) mmf
-
--- | Draw a card with replacement.
-replaceDraw :: Colour -> Colour -> State a -> State a
-replaceDraw original replacement s =
-  let s' = AI.draw [original] s
-      onTable  = M.update (\i -> if i > 1 then Just (i-1) else Nothing) original (AI.ontable s)
-      onTable' = M.alter (\case Just i -> Just (i+1); Nothing -> Just 1) replacement onTable
-  in s' { AI.ontable = onTable' }
+-- Utilities (Console)
 
 -- | Prompt for input, and parse it. Returns @Nothing@ if no input was given.
 prompt :: String -> (String -> Maybe a) -> InputT IO (Maybe a)
@@ -191,6 +198,85 @@ confirm = do
     Just str -> map toLower str `elem` ["y", "yes"]
     Nothing  -> False
 
+
+-------------------------------------------------------------------------------
+-- (Utilities) Pretty printing
+
+-- | Show a card and a quantity.
+showCards :: (Colour, Int) -> ShowS
+showCards (colour, num) = shows colour . showAsideNum num
+
+-- | Show a ticket, including its value.
+showTicket :: Show a => Ticket a -> ShowS
+showTicket (Ticket from to value) =
+  shows from . showString " -> " . shows to . showAsideNum value
+
+-- | Show an item in the plan, including its cost.
+showPlanItem :: Show a => (a, a, Label) -> ShowS
+showPlanItem (from, to, label) =
+  showTicket $ Ticket from to (lweight label)
+
+-- | Show a number in parentheses, after a space.
+showAsideNum :: Int -> ShowS
+showAsideNum num = showString " (" . shows num . showString ")"
+
+-- | Descriptive text for a move.
+showMove :: Show a => Move a -> ShowS
+showMove DrawLocomotiveCard =
+  showString "Draw a locomotive card from the table."
+showMove (DrawCards (Just c1) (Just c2))
+  | c1 == c2 = showString "Draw two " . shows c1 . showString " cards from the table."
+  | otherwise = showString "Draw a " . shows c1 . showString " and a " . shows c2 . showString " card from the table."
+showMove (DrawCards (Just c) _) =
+  showString "Draw a " . shows c . showString " card from the table and one from the deck."
+showMove (DrawCards _ _) =
+  showString "Draw two cards from the deck."
+showMove (ClaimRoute from to colour cards) =
+  showString "Build the " . shows colour . showString " route from " . shows from . showString " to " . shows to . showString " with " . showList cards . showString "."
+showMove DrawTickets = showString "Draw new tickets."
+
+
+-------------------------------------------------------------------------------
+-- (Utilities) Parsing
+
+-- | Parse a list.
+readWords :: Read a => String -> Maybe [a]
+readWords = traverse readMaybe . words
+
+-- | Parse a list of the given length.
+readWordsL :: Read a => Int -> String -> Maybe [a]
+readWordsL len str = case readWords str of
+  Just xs | length xs == len -> Just xs
+  _ -> Nothing
+
+-- | Parse tickets in the format \"From To Value\".
+readTickets :: Read a => String -> Maybe (NonEmpty (Ticket a))
+readTickets str = nonEmpty =<< go (words str) where
+  go (from:to:value:rest) =
+    let ticket = Ticket <$> readMaybe from <*> readMaybe to <*> readMaybe value
+    in (:) <$> ticket <*> go rest
+  go _ = Just []
+
+
+-------------------------------------------------------------------------------
+-- (Utilities) Output
+
+-- | Print a list of items in a single line.
+printList :: (a -> ShowS) -> String -> [a] -> InputT IO ()
+printList _ none [] = outputStrLn none
+printList f _ (x:xs) = do
+  outputStr (f x "")
+  mapM_ (\v -> outputStr . showString ", " . f v $ "") xs
+  outputStrLn ""
+
+-- | Print the plan, one route on each line, indented.
+printPlan :: Show a => State a -> InputT IO ()
+printPlan s = case AI.plan s of
+  (p:ps) -> do
+    outputStr . showString "Plan: " . showPlanItem p $ "\n"
+    mapM_ (\p' -> outputStr . showString "      " . showPlanItem p' $ "\n") ps
+  [] -> outputStrLn "No plan!"
+
 -- | Print the difference between two states.
 --
 -- Changes to the pending tickets displayed, as the user explicitly
@@ -203,20 +289,36 @@ printDiff old new = do
   when (length (AI.completedTickets new) > length (AI.completedTickets old)) $ do
     let newTickets = filter (`notElem` AI.completedTickets old) (AI.completedTickets new)
     outputStr "Just completed: "
-    printList outputStr showTicket "none!" newTickets
+    printList showTicket "none!" newTickets
 
   when (length (AI.missedTickets new) > length (AI.missedTickets old)) $ do
     let newTickets = filter (`notElem` AI.missedTickets old) (AI.missedTickets new)
     outputStr "Just missed: "
-    printList outputStr showTicket "none!" newTickets
+    printList showTicket "none!" newTickets
 
   when (AI.hand new /= AI.hand old) $ do
     outputStr "Cards in hand: "
-    printList outputStr showCard "none!" (M.toList $ AI.hand new)
+    printList showCards "none!" (M.toList $ AI.hand new)
 
   when (AI.ontable new /= AI.ontable old) $ do
     outputStr "Cards on table: "
-    printList outputStr showCard "none!" (M.toList $ AI.ontable new)
+    printList showCards "none!" (M.toList $ AI.ontable new)
 
   when (AI.plan new /= AI.plan old) $
-    printPlan outputStr new
+    printPlan new
+
+
+-------------------------------------------------------------------------------
+-- Utilities (Misc)
+
+-- | Like '>>=' but nicely dealing with @Maybe@-values.
+(.>=) :: Monad m => m (Maybe a) -> (a -> m (Maybe b)) -> m (Maybe b)
+mma .>= mmf = mma >>= maybe (pure Nothing) mmf
+
+-- | Draw a card with replacement.
+replaceDraw :: Colour -> Colour -> State a -> State a
+replaceDraw original replacement s =
+  let s' = AI.draw [original] s
+      onTable  = M.update (\i -> if i > 1 then Just (i-1) else Nothing) original (AI.ontable s)
+      onTable' = M.alter (\case Just i -> Just (i+1); Nothing -> Just 1) replacement onTable
+  in s' { AI.ontable = onTable' }
